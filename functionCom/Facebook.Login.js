1173c0643cf56910ec36d1ad6a649d49f653af99
nandakishore <104244494+Nandakishore695@users.noreply.github.com>
2022-08-22 15:20:54 +0530

    Update Facebook.Login.js

--- functionCom/Facebook.Login.js
+++ functionCom/Facebook.Login.js
@@ -1,11 +1,12 @@
 import React,{useState} from "react";
 
 function LoginPage() {
+  //Hooks to declare on top
   const [name,setName]=useState("");
   const [passw,setPassw]=useState("");
   const[dataInput, setDataInput]=useState(""); 
 
-
+ //onSubmit Handler Method
   const loginHandler = (e) => {
     e.preventDefault(); //for don't refresh form we use preventDefault
     const info={name:name,passw:passw}; 
@@ -19,7 +20,7 @@ function LoginPage() {
         <h3 style={{ color: "skyblue" }}>FaceBook</h3>
         <p>Log in to facebook</p>
         <form className="form" autoComplete="off" onSubmit={loginHandler}>
-          <input type="text" name="name" value={name} onChange={e=>setName(e.target.value)} placeholder="Email address or phone number" />
+          <input type="text" name="name" value={name} onChange={e=>setName(e.target.value)} placeholder="Email address or phone number" /> 
           <br />
           <input type="password" name="passw" value={passw} onChange={e=>setPassw(e.target.value)} placeholder="Password" />
           <br />
