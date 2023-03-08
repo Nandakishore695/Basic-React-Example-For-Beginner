13ea9d2b4eaa0e1ba7ef97a71a7ff746703ff514
Nandakishore Vishwanath <104244494+Nandakishore695@users.noreply.github.com>
2023-03-08 12:25:08 +0530

    Add files via upload

---
+++ functionCom/navigation header.js.tsx
@@ -0,0 +1,65 @@
+export default function App() {
+  return (
+    <div className="App">
+      <a className="nav-link" href={"/#home"}>
+        Home
+      </a>{" "}
+      <a className="nav-link" href={"/#roadmap"}>
+        Roadmap
+      </a>{" "}
+      <a className="nav-link" href={"/#aboutus"}>
+        AboutUs
+      </a>
+      <section className="banner-section " id="roadmap">
+        <div className="container">
+          <div className="row position-relative top-space">
+            <div className="yb-bg position-relative">
+              <h1 className="text-center">YellowBlock</h1>
+              <h1 className="text-center">is a collection of</h1>
+              <h1 className="text-center">10,000</h1>
+              <h1 className="text-right generative">generative</h1>
+            </div>
+          </div>
+        </div>
+      </section>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <section className="banner-section " id="home">
+        <div className="container">
+          <div className="row position-relative top-space">
+            <h1 className="text-center">Home</h1>
+          </div>
+        </div>
+      </section>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <section className="banner-section " id="aboutus">
+        <div className="container">
+          <div className="row position-relative top-space">
+            <h1 className="text-center">AboutUS</h1>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
